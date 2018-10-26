b385b7e8d7662fb80036c0d5886a8cf42069b11f
Appledayz <44154226+Appledayz@users.noreply.github.com>
2018-10-26 17:31:07 +0900

    [송동일] DBHelper클래스 외 1개 추가
    1. DBHelper 추가
    2. context.xml 추가

--- src/com/test/mymall/commons/DBHelper.java
+++ src/com/test/mymall/commons/DBHelper.java
@@ -5,14 +5,18 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
 public class DBHelper {
 	public static Connection getConnection() throws Exception {
-		
-		return null;
+		Context init = new InitialContext();
+		Connection conn = (Connection) init.lookup("java:comp/env/jdbc/mymall");
+		return conn;
 	}
-	public static void close(ResultSet rs, Statement stmt, Connection conn) throws SQLException {
-		if (rs!=null) {
-			rs.close();
-		}
+	public static void close(ResultSet rs, Statement stmt, Connection conn) {
+        if(rs != null) {try {rs.close();} catch(Exception e) {e.printStackTrace();}}
+        if(stmt != null) {try {stmt.close();} catch(Exception e) {e.printStackTrace();}}
+        if(conn != null) {try {conn.close();} catch(Exception e) {e.printStackTrace();}}
 	}
 }
